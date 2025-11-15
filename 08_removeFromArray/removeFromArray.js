00262f09a888e354b46a1484ea745ec2a3bedba1
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 08:19:36 -0700

    after ex 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    
+    for (let item of toRemove) {
+        const newArray = arr.filter(value => value !== item)
+        arr = newArray;
+    }
+
+    return arr
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3, 4, 5))
 // Do not edit below this line
 module.exports = removeFromArray;
