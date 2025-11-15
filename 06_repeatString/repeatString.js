81a4779137551ea30aaba705fd69816888ffeb26
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 07:55:55 -0700

    after ex 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num == 0) {
+    return "";
+  }
+  if (num >= 1) {
+    let newString = string
+    for (let i = 1; i < num; i++) {
+      newString += string;
+    }
+    return newString;
+  } else {
+    return "ERROR";
+  }
 };
+console.log(repeatString("hey", 2));
 
 // Do not edit below this line
 module.exports = repeatString;
