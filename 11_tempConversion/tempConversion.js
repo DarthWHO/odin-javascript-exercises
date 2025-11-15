2c9b57ae45aa29917bd53f5a1186a89b7a02ec36
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 10:50:22 -0700

    after ex 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+function round(value) {
+  if (Number.isInteger(value)) {
+    return value
+  }
+  return parseFloat(value.toFixed(1))
+}
+
+const convertToCelsius = function (temp) {
+  const converted = (temp - 32) * (5 / 9);
+  return round(converted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const converted = (temp * 9/5) + 32;
+  return round(converted)
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(73.2));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
