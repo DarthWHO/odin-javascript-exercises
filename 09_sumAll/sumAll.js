bff96e2840a7240f25204bc916b41e6cbde385c0
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 08:46:34 -0700

    after ex 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  let newFirst = first;
+  let newSecond = second;
+  let total = 0;
+
+  if (first > second) {
+    newFirst = second;
+    newSecond = first;
+  }
+
+  if (
+    newFirst < 0 ||
+    newSecond < 0 ||
+    !Number.isInteger(newFirst) ||
+    !Number.isInteger(newSecond)
+  ) {
+    return "ERROR";
+  }
+
+  if (newFirst === newSecond) {
+    return newFirst + newSecond;
+  }
+
+  while (newFirst <= newSecond) {
+    total += newFirst
+    newFirst++
+  }
+
+  return total
 
 };
 
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
