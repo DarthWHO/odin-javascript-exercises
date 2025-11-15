3b9fbcfb459998f2e97eeeb028c9803944491d06
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 08:58:06 -0700

    after ex 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
 
+    if (year % 4 !== 0) {
+        return isLeapYear;
+    } else if (year % 100 == 0 && year % 400 == 0) {
+        isLeapYear = true;
+        return isLeapYear;
+    } else if (year % 100 == 0) {
+        isLeapYear = false;
+        return isLeapYear
+    } 
+    else {
+        isLeapYear = true;
+        return isLeapYear;
+    }
 };
 
+console.log(leapYears(700))
+console.log(leapYears(1985))
 // Do not edit below this line
 module.exports = leapYears;
