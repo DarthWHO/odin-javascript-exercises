dc7b0514fcdc77ab66bfb20c11b7c87f9e0163c3
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 20:10:26 -0700

    after ex 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fib = [0, 1, 1]
+    if (num < 0) return "OOPS";
+
+    for (let i = 2; i < num; i++) {
+        fib.push(fib[i] + fib[i-1] )
+    }
+
+    return fib[num]
 
 };
+
+console.log(fibonacci(0))
+console.log(fibonacci(1))
+console.log(fibonacci(2))
+console.log(fibonacci(4))
+console.log(fibonacci(6))
 
 // Do not edit below this line
 module.exports = fibonacci;
