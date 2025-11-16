ac3cf37dcad5c36c97011ce93c027b8299537b82
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 20:34:28 -0700

    after all exercises complete

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestAge = 0;
+  let oldestPerson = {}
 
+  for (let person of people) {
+    let age = 0;
+    let currentYear = new Date().getFullYear();
+
+    if (!person.yearOfDeath) {
+      age = currentYear - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+  }
+  return oldestPerson;
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
