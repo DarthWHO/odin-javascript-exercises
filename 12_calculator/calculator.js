c748213b55083a8ae4d07e4ef60b8673b0b5a4d4
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 19:31:49 -0700

    after ex 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr[0] == undefined) {
+    return 0
+  }
+  return arr.reduce((total, value) => {
+    return total + value;
+  });
 };
 
-const multiply = function() {
+const multiply = function (arr) {
 
+  return arr.reduce((total, value) => {
+    return total * value;
+  });
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  // Handle negative numbers (optional)
+  if (n < 0) {
+    return -1; // Or throw an error
+  }
+
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
+
+sum([])
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +51,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
