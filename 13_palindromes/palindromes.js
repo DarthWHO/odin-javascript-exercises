65f5877e088ab92b0a322ca7cd3cdb6f2e39ad82
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-15 19:53:03 -0700

    after ex 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const reverseString = function (string) {
+  let newString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
 };
+
+const palindromes = function (phrase) {
+  const charsToIgnore = [" ", ",", ".", "?", "!"];
+  const phraseArray = phrase.split("");
+  const cleaned = phraseArray.filter((char) => !charsToIgnore.includes(char));
+
+  const cleanedString = cleaned.join("").toLowerCase();
+  if (cleanedString == reverseString(cleanedString)) {
+    return true;
+  } else return false;
+};
+
+console.log(palindromes("r3ace3car"));
+console.log(palindromes("tacos"));
 
 // Do not edit below this line
 module.exports = palindromes;
